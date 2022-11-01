d9dc68aebf8d40d9fa3905d9a25122839d3936e0
ELITEBOOK <ELITEBOOK@DESKTOP-7MJDDPC>
2022-11-01 19:38:13 +0100

    ex

--- Program.cs
+++ Program.cs
@@ -26,17 +26,65 @@ namespace ProgramTP2
             get { return datedebu; }   // get method
             set { datedebu = value; }  // set method
         }
+        public int Not  // property
+        {
+            get { return NotGlobale; }   // get method
+            set { NotGlobale = value; }  // set method
+        }
 
-       
+
+
+
 
 
 
     }
 
-   
+
+
+
+
+    class Quistion  // Base class
+    {
+
+        private string name;
+        private DateTime datedebut;
+        private int NotGlobale;
+
+
+
+        public Evaluation()
+        {
+            name = "exam C sharp";
+        }
+        public string Name   // property
+        {
+            get { return name; }   // get method
+            set { name = value; }  // set method
+        }
+        public DateTime datedebu  // property
+        {
+            get { return datedebu; }   // get method
+            set { datedebu = value; }  // set method
+        }
+        public int Not  // property
+        {
+            get { return NotGlobale; }   // get method
+            set { NotGlobale = value; }  // set method
+        }
+
+
+
+
+
 
 
     }
+
+
+
+
+}
 
 
 
