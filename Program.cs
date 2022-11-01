99b3deeb7116048e8fe0a7058d257a59ce5abf68
ELITEBOOK <ELITEBOOK@DESKTOP-7MJDDPC>
2022-11-01 10:05:48 +0100

    maryem

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Xml.Linq;
+
+namespace ProgramTP2
+{
+    class Evaluation   // Base class
+    {
+         private string name ;
+        public string Name   // property
+        {
+            get { return name; }   // get method
+            set { name = value; }  // set method
+        }
+
+
+
+    }
+}
+
